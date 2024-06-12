7e4604b547254e99e6cbe04da4320f7299cad861
Ahmed Hamza <81289670+ham-zax@users.noreply.github.com>
2024-06-12 19:40:35 +0000

    made interval change

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ PARAMS = {
     'minvCPUs': 1,
     'minStorage': 20
 }
-INTERVAL = 6  # Interval in seconds
+INTERVAL = 180  # Interval in seconds
 
 TELEGRAM_TOKEN = '7119782068:AAHm5qatChCeyaHdNmlt6FBGF8NxVxj0OV4'
 CHAT_ID = '376895924'
